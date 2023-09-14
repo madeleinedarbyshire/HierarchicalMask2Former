dda8bdf07779cfe61a990d9c004b8bce9609333c
Maddie <22774558+maddie157@users.noreply.github.com>
2023-09-14 14:00:02 +0100

    Measure inference speed

---
+++ measure_speed.py
@@ -0,0 +1,70 @@
+import argparse
+import torch
+from detectron2.config import get_cfg
+from detectron2.engine import DefaultPredictor
+from detectron2.data import MetadataCatalog
+from PIL import Image
+import time
+import numpy as np
+
+import detectron2.utils.comm as comm
+
+from detectron2.config import get_cfg
+from detectron2.engine import default_setup
+from detectron2.projects.deeplab import add_deeplab_config
+from detectron2.utils.logger import setup_logger
+from phenobench import PhenoBench
+
+# MaskFormer
+from mask2former import add_maskformer2_config
+
+
+parser = argparse.ArgumentParser(description="Example argparse parser for config and weights.")
+
+parser.add_argument("--config_file", type=str, help="Path to the configuration file")
+parser.add_argument("--weights", type=str, help="Path to the weights file")
+
+args = parser.parse_args()
+
+def warmup():
+    # Set the device to GPU (assuming you have a GPU available)
+    device = torch.device("cuda")
+
+    # Create a simple tensor and send it to the GPU
+    warm_up_tensor = torch.randn(1, 1).to(device)
+
+    # Perform some simple operations on the tensor
+    for _ in range(1000):
+        warm_up_tensor = torch.matmul(warm_up_tensor, warm_up_tensor)
+
+def setup(args):
+    """
+    Create configs and perform basic setups.
+    """
+    cfg = get_cfg()
+    # for poly lr schedule
+    add_deeplab_config(cfg)
+    add_maskformer2_config(cfg)
+    cfg.merge_from_file(args.config_file)
+    # cfg.merge_from_list(args.opts)
+    cfg.MODEL.WEIGHTS = args.weights
+    cfg.freeze()
+    default_setup(cfg, args)
+    # Setup logger for "mask_former" module
+    setup_logger(output=cfg.OUTPUT_DIR, distributed_rank=comm.get_rank(), name="mask2former")
+    return cfg
+
+cfg = setup(args)
+predictor = DefaultPredictor(cfg)
+data = PhenoBench("/workspace/PhenoBench", split="val", target_types=[], make_unique_ids=True)
+
+print('Warm up...')
+warmup()
+
+print('Run inference...')
+for d in data:
+    image = np.array(d["image"])
+    start_time = time.time()
+    outputs = predictor(image)
+    end_time = time.time()
+
